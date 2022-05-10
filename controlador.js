ac08ef6ac877889446e5917a823dc920f4dfd362
juan pablo aristizabal <juanarieda@gmail.com>
2022-05-10 17:12:12 -0500

    controlador.js

--- controlador.js
+++ controlador.js
@@ -1,108 +1,147 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"aceite vegetal",
+    precio:11500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/aceite%20vegetal.jfif?alt=media&token=2867cb73-3e43-4058-8b2e-fff9c7277a42"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"agua tonica",
+    precio:5000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/agua%20tonica.jfif?alt=media&token=d1a0c74a-eca5-4e73-bf4d-debe26418988"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"almendra de chocolate",
+    precio:4500,
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/almendra%20choco.jfif?alt=media&token=54a44095-e3d7-4ef5-b075-3e819a200d15"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"arepa de yuca",
+    precio:3000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/arepa%20de%20yuca.jfif?alt=media&token=84345fdf-6afb-439e-836c-4bbad15e904a"
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"atun en agua",
+    precio:6000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/atun%20agua.jfif?alt=media&token=3f7ea6a1-0eb8-464a-bad4-d7f42ae4d51a"
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"barquillo",
+    precio:2000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/barquillo.jfif?alt=media&token=e1f98d00-c45a-43a2-b925-1ad0c60e281b"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"chicharron",
+    precio:4000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/chicharron.jfif?alt=media&token=2471cf04-cd6f-46c8-addf-527c6f432836"
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"del valle",
+    precio:3500,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/del%20valle.jfif?alt=media&token=02145356-69bf-43fc-9d9d-970ff7177441"
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"denta stick",
+    precio:5000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/denta%20stick.jfif?alt=media&token=50bdbe0f-41c3-4afa-9cb6-93441c8b7bbf"
 }
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"galletas para gatos",
+    precio:3000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/galletas%20gatos.jfif?alt=media&token=8e61192d-3c64-42c4-96a1-6741302ebe0f"
 }
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"hueso de perro",
+    precio:2000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/hueso%20perros.jfif?alt=media&token=78d22f1b-7c32-416a-a9aa-d1fa16d33a33"
 }
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"lomito de tilapia",
+    precio:30000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/lomito%20de%20tilapia.jfif?alt=media&token=152056e2-ebf1-4225-9a2b-2854f05c5414"
 }
 
 let producto13={
-    nombre:"producto13",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"lomito de salmon",
+    precio:50000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/lomitos%20de%20salmon.jfif?alt=media&token=0d549b56-8f61-4536-b502-cd957d1f03e4"
 }
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"pack de chocorramo ",
+    precio:12000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/lonche%20ramo.jfif?alt=media&token=dcfc45d2-8df8-462c-9da1-625868b9c737"
 }
 
 let producto15={
-    nombre:"producto15",
-    precio:500000,
-    estado:"Nuevo",
-    foto:""
+    nombre:"nutrecan",
+    precio:7000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/nutrecan.jfif?alt=media&token=a969eca3-1f2e-4af3-a9e6-c7519a853940"
 }
+
+let producto16={
+    nombre:"pack de oreo",
+    precio:10000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/oreo%2B.jfif?alt=media&token=f6f28d1c-8a73-4cbd-bad0-a8d46e788029"
+}
+
+let producto17={
+    nombre:"pañitos para perro",
+    precio:5000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/pa%C3%B1os%20humedos.jfif?alt=media&token=0617244e-6246-49d8-a4e3-98cf36a701d5"
+}
+
+let producto18={
+    nombre:"tequila reposado",
+    precio:45000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/tequila%20reposado.jfif?alt=media&token=6d54b5c9-2be2-4607-af61-44687fc16ed6"
+}
+
+let producto19={
+    nombre:"vino tinto",
+    precio:25000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/vino%20tinto.jfif?alt=media&token=0668118b-a686-4772-9379-46fb0998f831"
+}
+
+let producto20={
+    nombre:"whiskas humedo",
+    precio:7000,
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-29fc6.appspot.com/o/whiskas%20humedo.jfif?alt=media&token=c1eeaa19-d833-43a1-9445-c04ef8e1eaf3"
+}
+
+
+
+
 
 //ARREGLO
 let productos=Array(
@@ -120,7 +159,12 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20
 )
 
 //RECORRO EL ARREGLO
